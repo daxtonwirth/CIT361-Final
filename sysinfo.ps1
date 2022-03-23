e5ac1b7b21d47972d2398387a26777d757ec3594
Daxton Wirth <66894542+daxtonwirth@users.noreply.github.com>
2022-03-23 09:41:43 +0900

    Update sysinfo.ps1

--- sysinfo.ps1
+++ sysinfo.ps1
@@ -1,44 +1,48 @@
-"------------------------------------------------------------------------------------------------------
-BASIC SYSTEM INFO
-------------------------------------------------------------------------------------------------------"
-systeminfo
-Get-NetIPaddress 
+"------------------------------------------------------------------------------------------------------"
+"BASIC SYSTEM INFO"
+# In order to start a security audit, we need to get the basic system info and these commands will do that"
+"------------------------------------------------------------------------------------------------------"
+# Get-ComputerInfo (This command corresponds to systeminfo in cmd)
+# This gives a lot of info we probably do not need. Here is one that filters out the info that is not as important:
+Get-ComputerInfo | Select-Object OsVersion, WindowsRegisteredOwner, CsDomainRole, csname, OsRegisteredUser, OsArchitecture, OsNumberOfUsers, OsNumberOfProcesses, OsMaxProcessMemorySize, OsName, CsModel
 
-"------------------------------------------------------------------------------------------------------
-DOMAIN INFO + other computers on network"
+"IP INFO" # Get-NetIPaddress 
+# This command can also have a lot of info so we can make it easier to read as a table adding "| ft", but still too much info so we can filter out unnecessary info:
+Get-NetIPaddress | sort ifIndex | Select-Object ifIndex, IPAddress, InterfaceAlias
+
+
+"------------------------------------------------------------------------------------------------------"
+"DOMAIN INFO + other computers on network"
+"------------------------------------------------------------------------------------------------------"
 get-adcomputer
 arp -a
 Invoke-Command -ComputerName $COMPUTERS -ScriptBlock {get-netipaddress | Select-Object PSComputerName, IPAddress}
 
-"------------------------------------------------------------------------------------------------------
-LISTENING PORTS:
-------------------------------------------------------------------------------------------------------"
+"------------------------------------------------------------------------------------------------------"
+"LISTENING PORTS:"
+"------------------------------------------------------------------------------------------------------"
 netstat -ano
 
-"------------------------------------------------------------------------------------------------------
-TEXT FILE THAT MAY HAVE PASSWORDS:"
-findstr /si password *.txt
-
-"------------------------------------------------------------------------------------------------------
-SCHEDULED TASKS"
+"------------------------------------------------------------------------------------------------------"
+"SCHEDULED TASKS"
+"------------------------------------------------------------------------------------------------------"
 schtasks /query /fo LIST /v
 
-"------------------------------------------------------------------------------------------------------
-WINDOWS DEFENDER STATUS"
+"------------------------------------------------------------------------------------------------------"
+"WINDOWS DEFENDER STATUS + FIREWALL RULES"
+"------------------------------------------------------------------------------------------------------"
 Get-service Windefend
 sc query windefend
 
-if (status -ne "running") {enable}
+# if (status -ne "running") {enable}
 
-"------------------------------------------------------------------------------------------------------
-UPDATES
-------------------------------------------------------------------------------------------------------"
-Update-MpSignature
-Get-WindowsUpdate -AcceptAll -Install
+"------------------------------------------------------------------------------------------------------"
+"UPDATES"
+"------------------------------------------------------------------------------------------------------"
+.\updates.ps1
 
-
-"------------------------------------------------------------------------------------------------------
-SCAN NETWORK FOR OPEN PORTS
-------------------------------------------------------------------------------------------------------"
+"------------------------------------------------------------------------------------------------------"
+"SCAN NETWORK FOR OPEN PORTS"
+"------------------------------------------------------------------------------------------------------"
 Import-Module .\port-scan.ps1
 port-scan-tcp 192.168.50.1 80
